11e3fc1aa03dbd3b17560c134a5f4debe4b0ae00
ZhanTop <1366562852@qq.com>
2020-07-09 08:46:15 +0800

    通用层搭建1

---
+++ RolePermissionSys/My.RolePermission.Ioc/SpringHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace My.RolePermission.Ioc
+{
+    public class SpringHelper
+    {
+        #region Spring容器上下文+IApplicationContext SpringContext
+        /// <summary>
+        /// Spring 容器上下文
+        /// </summary>
+        private static IApplicationContext SpringContext
+        {
+            get
+            {
+                return ContextRegistry.GetContext();
+            }
+        }
+        #endregion
+
+        #region 获取配置文件配置的对象 + T GetObject<T>(string objName) where T :class
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objName"></param>
+        /// <returns></returns>
+        public static T GetObject<T>(string objName) where T : class 
+        {
+            return (T)SpringContext.GetObject(objName);
+        }
+        #endregion
+    }
+}
